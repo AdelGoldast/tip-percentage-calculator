5f5e5d91929c33fe57ed65eb19c46e63d7eb16f4
Adel Goldast <adelgoldast@gmail.com>
2025-07-10 18:17:15 +0330

    add chart

---
+++ .ipynb_checkpoints/tip.v2-checkpoint.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('tip.csv')
+
+# چک کردن مقادیر خالی و جایگزینی آنها
+print(df.isnull().sum())
+df.fillna('-', inplace=True)
+
+#محاسبه میانگین انعام
+df ['Main tip'] = df['tip'] / df['total_bill']  * 100
+
+#میانگین ها
+mean_tip_by_sex = df.groupby('sex')['tip'].mean()
+
+mean_tip_per_day = df.groupby('day')['tip'].mean()
+
+mean_tip_by_time = df.groupby('time')['tip'].mean()
+
+#تنظیمات نمودار
+plt.figure(figsize=(10, 6))
+mean_tip_per_sex = df.groupby('sex')['tip'].mean().plot(kind='bar', color='skyblue')
+plt.title('Average Tip per sex')
+plt.xlabel('Sex')
+plt.ylabel('Tip')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+mean_tip_per_day = df.groupby('day')['tip'].mean().plot(kind='bar', color='skyblue')
+plt.title('Average Tip per Day of the Week')
+plt.xlabel('Day')
+plt.ylabel('Tip')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+mean_tip_per_time = df.groupby('time')['tip'].mean().plot(kind='bar', color='skyblue')
+plt.title('Average Tip per Time')
+plt.xlabel('Time')
+plt.ylabel('Tip')
+plt.show()
+
+
+print(df.head())
+
+
+
+
+
+
+
